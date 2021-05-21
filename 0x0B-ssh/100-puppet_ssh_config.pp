4de44dfed12756e01ded1664e25553ce8dd85313
Simon Sun <sunmingyu.97@gmail.com>
2021-05-21 23:40:57 +0000

    push

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,6 @@
 # Puppt
 class ssh::config {
-  file { ssh::params::ssh_service_config:
+  file { ssh::params::/etc/ssh/ssh_config:
     PasswordAuthentication => No
     StricHostKeyChecking => No
   }

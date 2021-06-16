18122e04099956fffe4879a6a3bfe32b49ef0e76
Simon Sun <sunmingyu.97@gmail.com>
2021-06-16 20:13:57 +0000

    first push

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,34 +1,40 @@
 #!/usr/bin/python3
-"""csv task """
-
-
+"""csv"""
 import csv
 import requests
 import sys
 
+
 def save_tasks_to_csv(employeeId):
+    """for api"""
+    # variables
     username = ''
     allTasks = []
 
-    userRes = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(employeeId))
-    todosRes = requests.get("https://jsonplaceholder.typicode.com/users/{}/todos".format(employeeId))
+    # do the get requests
+    usersRes = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}".format(employeeId))
+    todosRes = requests.get(
+        "https://jsonplaceholder.typicode.com/users/{}/todos".
+        format(employeeId))
 
-    username = userRes.json().get('username')
+    # get the json from responses
+    username = usersRes.json().get('username')
     todosJson = todosRes.json()
 
+    # loop through and save
     for task in todosJson:
         taskRow = []
         taskRow.append(employeeId)
         taskRow.append(username)
-        taskRow.append(task.get("completed"))
-        taskRow.append(task.get("title"))
-
+        taskRow.append(task.get('completed'))
+        taskRow.append(task.get('title'))
         allTasks.append(taskRow)
-        
-        with open("{}.csv".format(employeeId), 'w') as csvFile:
-            csvwriter = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
-            csvwriter.writerows(allTasks)
-        return 0
 
-if __name__ == "__main__":
+    with open("{}.csv".format(employeeId), 'w') as csvFile:
+        csvwriter = csv.writer(csvFile, quoting=csv.QUOTE_ALL)
+        csvwriter.writerows(allTasks)
+    return 0
+
+if __name__ == '__main__':
     save_tasks_to_csv(sys.argv[1])
